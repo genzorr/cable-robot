5abbe08f1e1716ed047005ad9eba476b84a21165
Michael Litvinov <korr237i@yandex.ru>
2022-03-10 01:30:39 +0300

    Added default setup

---
+++ src/cable_robot/cmake_commands.sh
@@ -0,0 +1,3 @@
+CMAKE_PREFIX_PATH=/opt/ros/rolling /usr/bin/cmake /home/genzorr/dev/cable-robot/src/cable_robot -DCMAKE_INSTALL_PREFIX=/home/genzorr/dev/cable-robot/install/cable_robot
+CMAKE_PREFIX_PATH=/opt/ros/rolling /usr/bin/cmake --build /home/genzorr/dev/cable-robot/build/cable_robot -- -j8 -l8
+CMAKE_PREFIX_PATH=/opt/ros/rolling /usr/bin/cmake --install /home/genzorr/dev/cable-robot/build/cable_robot
---
+++ src/cable_robot/src/general_control_unit.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("!! cable_robot package\n");
+  return 0;
+}
