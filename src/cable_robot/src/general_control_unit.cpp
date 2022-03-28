3ee43f2c83740d62cf5eaea831171f8b1a869336
Mikhail Litvinov <korr237i@yandex.ru>
2022-03-29 02:34:32 +0300

    Added logs

--- src/cable_robot/src/general_control_unit.cpp
+++ src/cable_robot/src/general_control_unit.cpp
@@ -5,12 +5,14 @@
 GCU::GCU()
 : Node("gcu"), x_(0.), y_(0.), z_(0.), xVel_(0.), yVel_(0.), zVel_(0.), controlTimerInterval_(200)
 {
+    RCLCPP_INFO(this->get_logger(), "Initializing General Control Unit");
     rclcpp::QoS qos(rclcpp::KeepLast(7));
     controlTimer_ = this->create_wall_timer(std::chrono::milliseconds(controlTimerInterval_), [this] { updateCallback(); });
     controlSub_ = this->create_subscription<geometry_msgs::msg::Twist>(
         "/cmd_vel", qos,
         std::bind(&GCU::controlCallback, this, std::placeholders::_1)
     );
+    RCLCPP_INFO(this->get_logger(), "General Control Unit initialized");
 }
 
 void GCU::controlCallback(const geometry_msgs::msg::Twist::ConstSharedPtr vel)
@@ -22,19 +24,16 @@ void GCU::controlCallback(const geometry_msgs::msg::Twist::ConstSharedPtr vel)
 
 void GCU::updateCallback()
 {
-    auto dt = (double)controlTimerInterval_;
+    auto dt = (double)controlTimerInterval_/1000;
     x_ += xVel_ * dt;
     y_ += yVel_ * dt;
     z_ += zVel_ * dt;
 
-    std::cout << x_ << " " << y_<< " " << z_ << std::endl;
+    RCLCPP_INFO(this->get_logger(), "x: %.4f, y: %.4f, z: %.4f", x_, y_, z_);
 }
 
 int main(int argc, char ** argv)
 {
-    // Add ros logs
-    std::cout << "General Control Unit" << std::endl;
-
     rclcpp::init(argc, argv);
     auto gcu = std::make_shared<GCU>();
     rclcpp::spin(gcu);
