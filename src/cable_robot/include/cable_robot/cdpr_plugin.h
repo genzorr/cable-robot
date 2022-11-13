d3b996b2b18f0588492e87a3d8009de070bff361
Mikhail Litvinov <korr237i@yandex.ru>
2022-11-14 02:49:41 +0300

    Change interface definition to a separate ros2 package

--- src/cable_robot/include/cable_robot/cdpr_plugin.h
+++ src/cable_robot/include/cable_robot/cdpr_plugin.h
@@ -1,62 +1,58 @@
 #ifndef CDPR_PLUGIN_H
 #define CDPR_PLUGIN_H
 
-#include <rclcpp/rclcpp.hpp>
-#include <gazebo_ros/node.hpp>
+#include "cable_robot_interfaces/msg/tensions.hpp"
 #include <gazebo/common/Plugin.hh>
-#include <gazebo/physics/Model.hh>
 #include <gazebo/physics/Link.hh>
-#include <sensor_msgs/msg/joint_state.hpp>
-#include <geometry_msgs/msg/accel.hpp>
+#include <gazebo/physics/Model.hh>
 #include <gazebo_msgs/msg/link_state.hpp>
-#include "../../../build/cable_robot/rosidl_generator_cpp/cable_robot/msg/tensions.hpp"
-#include "../../../build/cable_robot/rosidl_generator_cpp/cable_robot/msg/detail/tensions__struct.hpp"
+#include <gazebo_ros/node.hpp>
+#include <geometry_msgs/msg/accel.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 //#include "cable_robot/msg/tensions.hpp"
 
-namespace gazebo
-{
-    class CDPRPlugin : public ModelPlugin
-    {
-        struct Tension
-        {
-            ignition::math::Vector3d force;
-            ignition::math::Vector3d point;
-            std::string name;
-        };
+namespace gazebo {
+class CDPRPlugin : public ModelPlugin {
+  struct Tension {
+    ignition::math::Vector3d force;
+    ignition::math::Vector3d point;
+    std::string name;
+  };
 
-    public:
-        CDPRPlugin() : fMax(0.), controlReceived(false), cableJoints() {};
+public:
+  CDPRPlugin() : fMax(0.), controlReceived(false), cableJoints(){};
 
-        void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) override;
-        virtual void Update();
+  void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) override;
+  virtual void Update();
 
-    private:
-        void cableControlCallback(sensor_msgs::msg::JointState::ConstSharedPtr _msg)
-        {
-            tensionsCmd = *_msg;
-            controlReceived = true;
-        }
+private:
+  void cableControlCallback(sensor_msgs::msg::JointState::ConstSharedPtr _msg) {
+    tensionsCmd = *_msg;
+    controlReceived = true;
+  }
 
-    private:
-        double fMax;
-        bool controlReceived;
+private:
+  double fMax;
+  bool controlReceived;
 
-        gazebo_ros::Node::SharedPtr rosnode;
-        physics::ModelPtr model;
-        event::ConnectionPtr updateEvent;
+  gazebo_ros::Node::SharedPtr rosnode;
+  physics::ModelPtr model;
+  event::ConnectionPtr updateEvent;
 
-        std::vector<physics::JointPtr> cableJoints;    // gazebo cable prismatic joints
-        sensor_msgs::msg::JointState tensionsCmd;      // command received from ROS to control cables
+  std::vector<physics::JointPtr> cableJoints; // gazebo cable prismatic joints
+  sensor_msgs::msg::JointState
+      tensionsCmd; // command received from ROS to control cables
 
-        gazebo_msgs::msg::LinkState platformState;      // spatial state of platfrom
-        geometry_msgs::msg::Accel platformAccel;
-        physics::LinkPtr frameLink, platformLink;     // frame and platform Gazebo links
+  gazebo_msgs::msg::LinkState platformState; // spatial state of platfrom
+  geometry_msgs::msg::Accel platformAccel;
+  physics::LinkPtr frameLink, platformLink; // frame and platform Gazebo links
 
-        rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr tensionsSub;
-        rclcpp::Publisher<gazebo_msgs::msg::LinkState>::SharedPtr platformStatePub;
-        rclcpp::Publisher<geometry_msgs::msg::Accel>::SharedPtr platformAccelPub;
-    };
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr tensionsSub;
+  rclcpp::Publisher<gazebo_msgs::msg::LinkState>::SharedPtr platformStatePub;
+  rclcpp::Publisher<geometry_msgs::msg::Accel>::SharedPtr platformAccelPub;
+};
 
-    GZ_REGISTER_MODEL_PLUGIN(CDPRPlugin)
-}
+GZ_REGISTER_MODEL_PLUGIN(CDPRPlugin)
+} // namespace gazebo
 #endif // CDPR_PLUGIN_H
